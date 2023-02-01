f951608bae12f5ac1d2660eae77eee0a798e8a8b
RSG-GIT <solertirenso@gmail.com>
2023-02-01 17:41:53 -0600

    Leyendo HTML desde JavaScript-clase 10

--- Script.js
+++ Script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World")
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
+const parrafito = document.querySelector(".parrafito");
+const ide = document.querySelector("#ide");
+const input = document.querySelector("input");
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    ide,
+    input,
+});
