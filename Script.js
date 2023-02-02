447090856dc743d667c2f4416d1d959b95251b26
RSG-GIT <solertirenso@gmail.com>
2023-02-01 18:06:38 -0600

    Escribiendo HTML desde JavaScript-clase 11

--- Script.js
+++ Script.js
@@ -11,3 +11,24 @@ console.log({
     ide,
     input,
 });
+
+//Escribiendo HTML desde JavaScript
+
+h1.innerHTML = 'Patito <br> Feo';
+h1.innerText = 'Patito <br> Feo';
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class', 'rojo');
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
+
+input.value = "456"
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://static.platzi.com/static/images/conf/logo_black@2x.png');
+console.log(img);
+
+ide.innerHTML = "";
+ide.appendChild(img);
