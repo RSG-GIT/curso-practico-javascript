962b87ba15d9618d89d1e667fa6f64dd9b4dfb54
RSG-GIT <solertirenso@gmail.com>
2023-02-06 20:09:52 -0600

    Inicio de la clase 12 curso practico JavaScript

--- Script.js
+++ Script.js
@@ -1,34 +1,10 @@
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.querySelector(".parrafito");
-const ide = document.querySelector("#ide");
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    ide,
-    input,
-});
-
-//Escribiendo HTML desde JavaScript
-
-h1.innerHTML = 'AUTEC <br> Automatización Tecnológica';
-h1.innerText = 'AUTEC <br> Automatización Tecnológica';
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class', 'rojo');
-
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-// h1.classList.toggle('verde');
-// h1.classList.contains('verde');
-
-input.value = "Su Empresa de Tecnología"
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://static.platzi.com/static/images/conf/logo_black@2x.png');
-console.log(img);
-
-ide.innerHTML = ""; //de esta manera borro o modifico el <p id="ide">Esto es un parrafo con id</p> que esta en nuestro documento HTML//
-ide.appendChild(img);
+function btnOnClick() {
+    const sumaInputs = input1.value + " " + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
